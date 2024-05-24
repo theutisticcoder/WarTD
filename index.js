3f34890b631763f548ce3b731e5ab5c2a1449144
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-05-24 19:58:23 -0400

    add multi

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+var rooms = [];
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+app.use(express.static(__dirname));
+io.on('connection', (socket) => {
+  socket.on("join", r=> {
+    if(rooms.includes(r)){
+        socket.join(r);
+        socket.to(r).emit("opengame");
+    }
+    else{
+        socket.emit("no");
+    }
+  })
+  socket.on("start", r=> {
+    if(rooms.includes(r)){
+       
+        socket.emit("taken");
+    }
+    else{
+        rooms.push(r);
+        socket.emit("success");
+        socket.join(r);
+    }
+  })
+  socket.on("et1", (t)=> {
+    socket.to(Array.from(socket.rooms)[1]).emit("et1", t);
+  })
+  socket.on("fire", ()=> {
+    socket.to(Array.from(socket.rooms)[1]).emit("fire");
+  })
+  socket.on("defend", ()=> {
+    socket.to(Array.from(socket.rooms)[1]).emit("defend");
+  })
+});
+
+server.listen(3000, () => {
+  console.log('server running at 3000');
+});
